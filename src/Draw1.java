9f721b2eafcdec78fa671aa78c55069f62378919
webflyer <codemaster041@gmail.com>
2021-09-22 22:41:59 +0530

    a marvellous abode design created through javafx

---
+++ src/Draw1.java
@@ -0,0 +1,36 @@
+import java.awt.*;
+import java.awt.event.*;
+public class Draw1 extends Frame{
+Draw1(){
+	this.addWindowListener(new WindowAdapter()
+			{
+		public void windowClosing(WindowEvent e) {
+			System.exit(0);
+		}
+			});
+}
+	public void paint(Graphics g)
+	{
+		g.setColor(Color.blue);
+		
+		g.drawRect(40,40,200,200);
+		
+		g.drawOval(90,70,80,80);
+		
+		g.drawOval(110,95,5,5);
+		g.drawOval(145, 95, 5, 5);
+		
+		g.drawLine(130,95, 130, 115);
+		
+		g.drawArc(113,115, 35,20,0,-180);
+	}
+	public static void main(String[] args) {
+		Draw1 d = new Draw1();
+		
+		d.setSize(400,400);
+		d.setTitle("My drawing");
+		
+		d.setVisible(true);
+	}
+}
+
---
+++ src/Draw2.java
@@ -0,0 +1,39 @@
+import java.awt.*;
+import java.awt.event.*;
+public class Draw2 extends Frame{
+Draw2(){
+	this.addWindowListener(new WindowAdapter() {
+		public void windowClosing(WindowEvent e) {
+			System.exit(0);
+		}
+	});
+}
+public void paint(Graphics g){
+	g.setColor(Color.blue);
+	g.fillRect(40,40,200,200);
+	
+	g.setColor(Color.yellow);
+	
+	g.fillOval(90,70,80,80);
+	
+	g.setColor(Color.black);
+	
+	g.fillOval(110,95,5,5);
+	
+	g.fillOval(145,95,5,5);
+	
+	g.drawLine(130,95,130,115);
+	
+	g.setColor(Color.red);
+	
+	g.fillArc(113,115,35,20,0,-180);
+}
+	public static void main(String args[]) {
+		Draw2 d = new Draw2();
+		d.setSize(400,400);
+		
+		d.setTitle("My Drawing");
+		d.setVisible(true);
+	}
+}
+
